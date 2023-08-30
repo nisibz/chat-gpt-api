15abb68d4f64cb012c5dcc5decf83a2e7bbef915
nantachai <bz.nantachai@gmail.com>
2023-08-30 23:10:24 +0700

    Refactor code, add history functionality

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
-import { config } from "dotenv"
-config()
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
+import fs from "fs";
+import { promisify } from "util";
 
-import { Configuration, OpenAIApi } from "openai"
-import readline from "readline"
+config();
 
-const openAi = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
-  })
-)
+const openAi = createOpenAIApi(process.env.OPEN_AI_API_KEY);
+const userInterface = createReadlineInterface();
+const currentDate = new Date().toISOString().split("T")[0];
+const HISTORY_DIR = "history";
+const HISTORY_FILE_PATH = `${HISTORY_DIR}/${currentDate}.json`;
 
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+let inMemoryHistory = [];
 
-userInterface.prompt()
-userInterface.on("line", async input => {
+function createOpenAIApi(apiKey) {
+  return new OpenAIApi(new Configuration({ apiKey }));
+}
+
+function createReadlineInterface() {
+  return readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+}
+
+async function initializeHistory() {
+  try {
+    inMemoryHistory = JSON.parse(
+      await promisify(fs.readFile)(HISTORY_FILE_PATH, "utf-8")
+    );
+  } catch (err) {
+    if (err.code === "ENOENT") inMemoryHistory = [];
+    else throw err;
+  }
+}
+
+async function ensureHistoryDirectoryExists() {
+  if (!fs.existsSync(HISTORY_DIR)) {
+    await promisify(fs.mkdir)(HISTORY_DIR);
+  }
+}
+
+async function saveToHistory() {
+  await promisify(fs.writeFile)(
+    HISTORY_FILE_PATH,
+    JSON.stringify(inMemoryHistory, null, 2),
+    "utf-8"
+  );
+}
+
+async function getOpenAIResponse(input) {
   const response = await openAi.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choices[0].message.content)
-  userInterface.prompt()
-})
+  });
+  return response.data.choices[0].message.content;
+}
+
+userInterface.on("line", async (input) => {
+  try {
+    process.stdout.write("Loading...");
+    const output = await getOpenAIResponse(input);
+    process.stdout.clearLine(); // Clear the current line
+    process.stdout.cursorTo(0); // Move the cursor to the beginning of the line
+    console.log(output);
+
+    inMemoryHistory.push({ input, output });
+    await saveToHistory();
+
+    userInterface.prompt();
+  } catch (err) {
+    console.error("An error occurred:", err);
+    userInterface.prompt();
+  }
+});
+
+async function initializeApplication() {
+  await ensureHistoryDirectoryExists();
+  await initializeHistory();
+  userInterface.prompt();
+}
+
+initializeApplication();
